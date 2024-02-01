681daee5f265ed07f82e5662ad5b7a267b719a10
GrimCLown <ranack21@gmail.com>
2024-02-01 23:23:10 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
-import Header from './componets/Header/header'
-import Nav from './componets/Nav/nav'
-import About from './componets/About/about'
-import Experience from './componets/Experience/experience'
-import Contacts from './componets/Contacts/contacts'
-import Portfolio from './componets/portfolio/portfolio'
-import Services from './componets/Services/services'
-import Footer from './componets/Footer/footer'
+import React from "react";
+import Header from "./componets/Header/header";
+import Nav from "./componets/Nav/nav";
+import About from "./componets/About/about";
+import Experience from "./componets/Experience/experience";
+import Contacts from "./componets/Contacts/contacts";
+import Portfolio from "./componets/portfolio/portfolio";
+import Services from "./componets/Services/services";
+import Footer from "./componets/Footer/footer";
 
 const App = () => {
   return (
     <>
-        <Nav />
-        <Header />
-        <About />
-        <Experience />
-        <Contacts/>
-        <Footer />
+      <Nav />
+      <Header />
+      <About />
+      <Experience />
+      <Services />
+      <Contacts />
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
