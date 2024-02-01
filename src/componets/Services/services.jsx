b376afeea234b6ac56f82dab5b5aa4cd13ea6127
GrimCLown <ranack21@gmail.com>
2024-02-01 23:12:20 +0800

    Update services.jsx

--- src/componets/Services/services.jsx
+++ src/componets/Services/services.jsx
@@ -1,10 +1,114 @@
-import React from 'react'
-import './services.css'
+import React from "react";
+import "./services.css";
 
 const services = () => {
-  return (
-    <section id='services'>services</section>
-  )
-}
+  return;
+  <section id="services">
+    <h2 className="getTO"> Skills</h2>
 
-export default services
+    <div className="container">
+      <article className="skill_card">
+        <div className="skill-box">
+          <h2>HTML</h2>
+          <div className="skill-bar">
+            <span className="skill-per">
+              <span className="tooltip">70%</span>
+            </span>
+          </div>
+        </div>
+
+        <div className="skill-box">
+          <h2>CSS</h2>
+          <div className="skill-bar">
+            <span className="skill-per ">
+              <span className="tooltip">70%</span>
+            </span>
+          </div>
+        </div>
+
+        <div className="skill-box">
+          <h2>Bootstrap</h2>
+          <div className="skill-bar">
+            <span className="skill-per sixty">
+              <span className="tooltip">60%</span>
+            </span>
+          </div>
+        </div>
+
+        <div className="skill-box">
+          <h2>JavaScript</h2>
+          <div className="skill-bar">
+            <span className="skill-per sixty">
+              <span className="tooltip">60%</span>
+            </span>
+          </div>
+        </div>
+
+        <div className="skill-box">
+          <h2>jQuery</h2>
+          <div className="skill-bar">
+            <span className="skill-per fifty">
+              <span className="tooltip">50%</span>
+            </span>
+          </div>
+        </div>
+
+        <div className="skill-box">
+          <h2>Php</h2>
+          <div className="skill-bar">
+            <span className="skill-per fifty">
+              <span className="tooltip">50%</span>
+            </span>
+          </div>
+        </div>
+
+        <div className="skill-box">
+          <h2>Java</h2>
+          <div className="skill-bar">
+            <span className="skill-per sixty">
+              <span className="tooltip">60%</span>
+            </span>
+          </div>
+        </div>
+
+        <div className="skill-box">
+          <h2>C</h2>
+          <div className="skill-bar">
+            <span className="skill-per sixty">
+              <span className="tooltip">60%</span>
+            </span>
+          </div>
+        </div>
+
+        <div className="skill-box">
+          <h2>C++</h2>
+          <div className="skill-bar">
+            <span className="skill-per sixty">
+              <span className="tooltip">60%</span>
+            </span>
+          </div>
+        </div>
+
+        <div className="skill-box">
+          <h2>Laravel</h2>
+          <div className="skill-bar">
+            <span className="skill-per fifty">
+              <span className="tooltip">50%</span>
+            </span>
+          </div>
+        </div>
+
+        <div className="skill-box">
+          <h2>Sql</h2>
+          <div className="skill-bar">
+            <span className="skill-per fifty">
+              <span className="tooltip">50%</span>
+            </span>
+          </div>
+        </div>
+      </article>
+    </div>
+  </section>;
+};
+
+export default services;
