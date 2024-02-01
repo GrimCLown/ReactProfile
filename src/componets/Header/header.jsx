b05f3f9835af12edd6c1dc92c5beeb3c11afa59d
GrimCLown <ranack21@gmail.com>
2024-02-01 23:15:42 +0800

    update services

--- src/componets/Header/header.jsx
+++ src/componets/Header/header.jsx
@@ -1,29 +1,30 @@
-import React from 'react'
-import './header.css'
-import CTA from './CTA'
-import ME from '../../assets/ck.png'
-import Socials from './Socials'
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/ck.png";
+import Socials from "./Socials";
 
 const header = () => {
   return (
     <header>
       <div className="container header_container">
         <h5>Hello I'm</h5>
-        <h1>Calvin Klein</h1>
-        <h5 className='text-light'>Fullstack Developer (Fresher)</h5>
+        <h1>Calvin Klein&</h1>
+        <h5 className="text-light">Fullstack Developer (Fresher)</h5>
         <CTA />
-        <Socials/>
+        <Socials />
 
-        <a href="" className='scroll_down'>Scroll Down</a>
-        <div className='me'>
+        <a href="" className="scroll_down">
+          Scroll Down
+        </a>
+        <div className="me">
           <img src={ME} alt="me" />
         </div>
 
-        <a href="#contact" className='Scroll Down'></a>
+        <a href="#contact" className="Scroll Down"></a>
       </div>
-
     </header>
-  )
-}
+  );
+};
 
-export default header
+export default header;
